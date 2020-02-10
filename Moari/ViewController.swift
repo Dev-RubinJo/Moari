09a1d08c81295b16ec7d3a97385f67a7abc03497
Yoobin JO <dbqlsdl1234@naver.com>
2020-02-10 23:41:50 +0900

    기본 프로젝트 생성

---
+++ Moari/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moari
+//
+//  Created by YooBin Jo on 2020/02/10.
+//  Copyright © 2020 YooBin Jo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
