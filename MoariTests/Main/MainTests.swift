382857d4fd838530c8d799900591c58f2c82f502
Yoobin JO <dbqlsdl1234@naver.com>
2020-02-11 18:18:38 +0900

    main 도메인 테스트코드 작성 완료

--- MoariTests/Main/MainTests.swift
+++ MoariTests/Main/MainTests.swift
@@ -6,4 +6,45 @@
 //  Copyright © 2020 YooBin Jo. All rights reserved.
 //
 
-import Foundation
+import XCTest
+@testable import Moari
+
+class MainTests: XCTestCase {
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testMakeMain() {
+        self.measure {
+            let vc = MainVC.viewRouter.makeMainVC()
+            XCTAssertNotNil(vc)
+        }
+    }
+    
+    func testMainVCActorIsNil() {
+        self.measure {
+            let vc = MainVC.viewRouter.makeMainVC()
+            XCTAssertNotNil(vc.actor)
+        }
+    }
+    
+    func testMainActorDataManagerIsNil() {
+        let actor = MainVC.viewRouter.makeMainVC().actor
+        XCTAssertNotNil(actor?.dataManager)
+        
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
+
