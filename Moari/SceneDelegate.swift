9875d7edabdc6d4560a91c708cd1baa4c78806ae
Yoobin JO <dbqlsdl1234@naver.com>
2020-02-11 18:15:56 +0900

    SceneDelegate 파일에 root viewController 설정

--- Moari/SceneDelegate.swift
+++ Moari/SceneDelegate.swift
@@ -8,16 +8,22 @@
 
 import UIKit
 
+@available(iOS 13.0, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
+    private let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow.init(frame: UIScreen.main.bounds)
+        self.window?.windowScene = windowScene
+        self.window?.rootViewController = SplashVC.viewRouter.makeSplashVC()
+        self.window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
