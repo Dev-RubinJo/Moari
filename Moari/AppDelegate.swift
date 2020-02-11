00ec221949f01363f6c938a41f4a4dd0d81cef14
Yoobin JO <dbqlsdl1234@naver.com>
2020-02-11 18:17:42 +0900

    AppDelegate, Info.plist 설정

--- Moari/AppDelegate.swift
+++ Moari/AppDelegate.swift
@@ -8,31 +8,53 @@
 
 import UIKit
 import Firebase
+import BackgroundTasks
+import AlamofireNetworkActivityIndicator
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
+        NetworkActivityIndicatorManager.shared.isEnabled = true
+        
+        if #available(iOS 13.0, *) {}
+        else {
+            self.window = UIWindow.init(frame: UIScreen.main.bounds)
+            self.window?.rootViewController = SplashVC.viewRouter.makeSplashVC()
+            self.window?.makeKeyAndVisible()
+        }
         return true
     }
-
+    // 백그라운드 테스트를 위해 준비
+//    @available(iOS 13.0, *)
+//    private func registerBackgroundTasks() {
+//
+//        BGTaskScheduler.shared.register(forTaskWithIdentifier: "", using: nil) { task in
+//            self.test()
+//        }
+//    }
+//    func test() {}
+    
     // MARK: UISceneSession Lifecycle
-
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    
 }
 
