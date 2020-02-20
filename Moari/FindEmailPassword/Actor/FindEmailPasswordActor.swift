d7f0ac5d16d3d44f130fd2a180e4484f0a372864
Yoobin JO <dbqlsdl1234@naver.com>
2020-02-20 12:04:41 +0900

    FindEmailPassword 도메인 기본 파일 생성 완료

---
+++ Moari/FindEmailPassword/Actor/FindEmailPasswordActor.swift
@@ -0,0 +1,9 @@
+//
+//  FindEmailPasswordActor.swift
+//  Moari
+//
+//  Created by YooBin Jo on 2020/02/20.
+//  Copyright © 2020 YooBin Jo. All rights reserved.
+//
+
+import Foundation
---
+++ Moari/FindEmailPassword/View/FindEmailPasswordVC.swift
@@ -0,0 +1,16 @@
+//
+//  FindEmailPasswordVC.swift
+//  Moari
+//
+//  Created by YooBin Jo on 2020/02/20.
+//  Copyright © 2020 YooBin Jo. All rights reserved.
+//
+
+import UIKit
+
+class FindEmailPasswordVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
