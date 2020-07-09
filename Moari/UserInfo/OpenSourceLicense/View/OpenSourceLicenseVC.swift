3fbc63347c33cb67607e1a4b10e62fbe791893eb
Yoobin JO <dbqlsdl1234@naver.com>
2020-07-09 15:03:32 +0900

    오픈소스 라이브러리 화면 추가를 위한 준비

---
+++ Moari/UserInfo/OpenSourceLicense/View/OpenSourceLicenseVC.swift
@@ -0,0 +1,18 @@
+//
+//  OpenSourceLicenseVC.swift
+//  Moari
+//
+//  Created by YooBin Jo on 2020/04/08.
+//  Copyright © 2020 YooBin Jo. All rights reserved.
+//
+
+import UIKit
+
+class OpenSourceLicenseVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
